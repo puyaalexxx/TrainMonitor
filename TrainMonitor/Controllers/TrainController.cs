b17934d42f2a7ea760c05b8288d0651c26f57a9f
Alex <puyaalexxx@gmail.com>
2025-10-02 11:13:01 +0300

    styled Trains page data

--- TrainMonitor/Controllers/TrainController.cs
+++ TrainMonitor/Controllers/TrainController.cs
@@ -8,6 +8,25 @@ public class TrainController : Controller
 
     public IActionResult GetTrains()
     {
+        ViewBag.Title = "Trains";
+
+        // Read JSON file
+        //string json = System.IO.File.ReadAllText("trains.json");
+
+        // Deserialize only the relevant part
+        //var root = JsonSerializer.Deserialize<Root>(json); // Root contains "data": List<TrainJson>
+
+        /*var trains = _context.Trains
+            .Select(t => new TrainViewModel
+            {
+                Id = t.Id,
+                TrainName = t.TrainName,
+                TrainNumber = t.TrainNumber,
+                DelayTime = t.DelayTime,
+                NextStation = t.DepartureTime.AddMinutes(t.DelayTime).ToString("HH:mm"),
+                HasIncidentHistory = t.Incidents.Any()
+            }).ToList();*/
+
         return View();
     }
 }
---
+++ TrainMonitor/ViewModels/TrainViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainMonitor.ViewModels;
+
+public class TrainViewModel
+{
+    public int Id { get; set; }
+    public string TrainName { get; set; }
+    public string TrainNumber { get; set; }
+    public int DelayTime { get; set; }
+    public string NextStation { get; set; }
+    public bool HasIncidentHistory { get; set; }
+}
