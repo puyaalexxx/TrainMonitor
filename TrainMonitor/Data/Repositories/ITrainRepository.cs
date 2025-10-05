a3004f58de18da865e4c8de5b523b68b67654af5
Alex <puyaalexxx@gmail.com>
2025-10-05 18:03:47 +0300

    added train repositories

---
+++ TrainMonitor/Data/Repositories/ITrainRepository.cs
@@ -0,0 +1,10 @@
+﻿using TrainMonitor.Models;
+
+namespace TrainMonitor.Data.Repositories;
+
+public interface ITrainRepository
+{
+    Task<Train?> GetByIdAsync(string trainId);
+    Task AddAsync(Train train);
+    Task SaveChangesAsync();
+}
---
+++ TrainMonitor/Data/Repositories/TrainRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainMonitor.DataBase;
+using TrainMonitor.Models;
+
+namespace TrainMonitor.Data.Repositories;
+
+public class TrainRepository : ITrainRepository
+{
+    private readonly ApplicationDbContext _context;
+    public TrainRepository(ApplicationDbContext context) => _context = context;
+
+    public Task<Train?> GetByIdAsync(string trainId) => _context.Trains.FirstOrDefaultAsync(t => t.Id == trainId);
+
+    public Task AddAsync(Train train) => _context.Trains.AddAsync(train).AsTask();
+
+    public Task SaveChangesAsync() => _context.SaveChangesAsync();
+}
